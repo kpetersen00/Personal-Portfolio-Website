a86fa6cf3e544b7012538fc5bd93b423ea456ffa
Kyle Petersen <kyle-petersen@Kyles-MacBook-Air.local>
2022-04-06 15:53:21 -0700

    server.js added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(express.static('assets'));
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const listener = app.listen(process.env.PORT || 5500, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
